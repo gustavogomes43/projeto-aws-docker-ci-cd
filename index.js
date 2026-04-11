bac310ae6ca8279518659767a47b0125a84ae379
gustavogomes43 <gustavodg30@gmail.com>
2026-04-11 03:56:02 +0000

    style: atualizando texto para Amazon EC2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const http = require('http');
+Amazon EC2const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Projeto 1: Docker CI/CD na AWS com App Runner - Funcionando!\n');
+  res.end('Projeto 1: Docker CI/CD na AWS com App Amazon EC2 - Funcionando!\n');
 });
 server.listen(8080, '0.0.0.0');
